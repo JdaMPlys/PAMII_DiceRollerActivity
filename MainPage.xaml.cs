e066f1d2e5bb62794ab7eb344233e7c2eaa4b97a
Julian David <147755067+JdaMPlys@users.noreply.github.com>
2024-04-02 12:45:36 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace DiceRollerActivity
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSortear(object sender, EventArgs e)
+        {
+            double numeroDado = SortearDado();
+            sorteadoDado.Text = numeroDado.ToString();
+        }
+        
+        private double SortearDado()
+        {
+            double valorDado = Convert.ToDouble(ValueDice.Text);
+            if (int.TryParse(ValueDice.Text, out int limite))
+            {
+                Random rand = new Random();
+                int numeroSorteado = rand.Next(1, limite + 1);
+                valorDado = numeroSorteado;
+            }
+            return valorDado;
+
+        }
+    }
+
+}
